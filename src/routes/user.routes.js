17d023ac81e1abfd09ac68d694d43b9fe7200baa
Gaurav Sharma <gaurav130706@gmail.com>
2026-01-27 16:53:56 +0530

    add user account management routes for password change, current user retrieval, and profile updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,8 +23,14 @@ router.route("/login").post(loginUser)
 
 //Secured Routes
 router.route("/logout").post(verifyJWT, logoutUser) //VerifyJWT is a middelwaare, in the aruments inside post it is written 1st there executed first, than the next() inside it tells that now run the next one
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)  //isko galti se post mat rakh dena
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar) //1st we use verifyJWT to verify user than use upload.single("avatar") and here we used single because we are uplodaing only one file
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)// here in this controller we got data from URL and there we declared username therefore here also we must use username
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 
