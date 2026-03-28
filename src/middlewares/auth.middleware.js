2fc29cbc413cd8918d92bda22785dbdc8e45f29b
Gaurav Sharma <gaurav130706@gmail.com>
2026-03-28 21:26:57 +0530

    fix: correct import path casing for APIerror in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { APIerror } from "../utils/ApiError.js";
+import { APIerror } from "../utils/APIerror.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import {User} from "../models/user.model.js"
