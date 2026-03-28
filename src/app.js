3a914398542a9b4bda008dba1893353611149f73
Gaurav Sharma <gaurav130706@gmail.com>
2026-03-28 19:26:13 +0530

    add: implement routing for likes, comments, healthcheck, and dashboard; + debugging

--- src/app.js
+++ src/app.js
@@ -27,6 +27,10 @@ import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import subscryptionRouter from "./routes/subscryption.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 //routes declaration
 app.use("/api/v1/users",userRouter)     ///api/v1/users par jate hi userRouter is activated and control transfered to user.routes.js    
@@ -38,6 +42,14 @@ app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscryptions", subscryptionRouter)
 
 app.use("/api/v1/playlists", playlistRouter)
+
+app.use("/api/v1/likes", likeRouter)
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
+app.use("/api/v1/dashboard", dashboardRouter)
+
+app.use("/api/v1/comments", commentRouter)
 
 
 
